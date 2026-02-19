53599a961c574f31596f7134b2e16ef574b9083c
DelyBunn <delybunn@gmail.com>
2026-02-19 16:40:24 -0500

    windows build file

---
+++ build.bat
@@ -0,0 +1,3 @@
+python -m SCons platform=windows target=template_debug arch=x86_64 d3d12=no
+python -m SCons platform=windows target=template_release arch=x86_64 d3d12=no
+python -m SCons platform=windows d3d12=no
